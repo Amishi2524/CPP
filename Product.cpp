9479a185b8cc5d1c2ab013493983607eb4fe8e8e
Amishi2524 <78739125+Amishi2524@users.noreply.github.com>
2021-07-05 20:24:30 +0530

    Product of 2 nos. entered by user

---
+++ Product.cpp
@@ -0,0 +1,16 @@
+// product of 2 nos.
+#include<iostream>
+using namespace std;
+int main()
+{
+    double no1, no2, product;
+    cout <<"****************************************" << endl;
+    cout << "Enter number 1: ";
+    cin >> no1;
+    cout << "Enter number 2: ";
+    cin >> no2;
+    product = no1* no2;
+    cout <<endl << "Product of " << no1 <<" and "<<no2<<" is : "<<product << endl;
+    cout <<"****************************************" << endl;
+    return 0;
+}
